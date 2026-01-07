31789544749392faf290d5325c017651614fc7b6
Tarunjeet Singh <54957022+tarunjeetsingh1991@users.noreply.github.com>
2026-01-07 13:02:18 -0500

    Delete HotelService directory

--- HotelService/src/main/java/com/lcwd/hotel/respositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lcwd.hotel.respositories;
-
-import com.lcwd.hotel.entites.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}

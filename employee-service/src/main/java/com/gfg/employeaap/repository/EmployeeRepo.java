7d10f338d9c66e07dfe19cd3dec813a38d944a23
Tarunjeet Singh <54957022+tarunjeetsingh1991@users.noreply.github.com>
2026-01-07 13:05:30 -0500

    Delete employee-service directory

--- employee-service/src/main/java/com/gfg/employeaap/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.gfg.employeaap.repository;
-
-import com.gfg.employeaap.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> 
-{
-
-}

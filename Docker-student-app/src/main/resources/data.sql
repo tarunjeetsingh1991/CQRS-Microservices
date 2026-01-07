9c614e9d4e08644c2080bdc42f8ffe8df1672479
Tarunjeet Singh <54957022+tarunjeetsingh1991@users.noreply.github.com>
2026-01-07 13:02:10 -0500

    Delete Docker-student-app directory

--- Docker-student-app/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO student (name, age) VALUES ('Tarunjeet',34);
-INSERT INTO student (name, age) VALUES ('Gurchintan',32);
-INSERT INTO student (name, age) VALUES ('Japneet',26);
-INSERT INTO student (name, age) VALUES ('Inderjit',58);

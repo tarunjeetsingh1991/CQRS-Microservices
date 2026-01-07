66491630f042dd089625ad0a8537fd1f79c103e2
Tarunjeet Singh <54957022+tarunjeetsingh1991@users.noreply.github.com>
2026-01-07 13:06:19 -0500

    Delete student_restAPI directory

--- student_restAPI/src/main/java/com/practice/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.practice.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>
-{
-
-}

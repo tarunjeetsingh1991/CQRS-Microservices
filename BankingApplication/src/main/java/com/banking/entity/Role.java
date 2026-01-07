fa9b3628487d8fd19b85913907d4254b530a4343
Tarunjeet Singh <54957022+tarunjeetsingh1991@users.noreply.github.com>
2026-01-07 13:01:47 -0500

    Delete BankingApplication directory

--- BankingApplication/src/main/java/com/banking/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.banking.entity;
-
-public enum Role {
-    ADMIN,
-    USER;
-}
--- BankingApplication/src/main/java/com/banking/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.banking.entity.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
